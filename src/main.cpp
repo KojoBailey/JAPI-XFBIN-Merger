e48efe03c4a2b7cfd692b54a9f12ee2023661e5a
Kojo Bailey <kojomolojo@gmail.com>
2024-08-05 04:06:28 +0100

    Organise

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,16 @@
 #include "main.h"
 
-#include <stdio.h>
-#include <cstdint>
-#include <sstream>
-
 // This function is called when the mod is loaded.
 // It should return a ModMeta struct with the mod's information.
 ModMeta __stdcall GetModInfo() {
     static ModMeta meta = {
-        "PlayerColorParam", // Name
+        "PlayerColorParam Reader", // Name
         "PlayerColorParam", // GUID
         "1.0.0", // Version
         "Kojo Bailey" // Author
     };
 
     return meta;
-}
-
-typedef std::uint64_t*(__fastcall* Load_nuccBinary_t)(const char*, const char*);
-Load_nuccBinary_t Load_nuccBinary_original;
-
-std::uint64_t* __fastcall Load_nuccBinary(const char* a1, const char* a2) {
-    return Load_nuccBinary_original(a1, a2);
-}
-
-typedef std::uint64_t(__fastcall* NUCC_Encrypt_t)(const char*);
-NUCC_Encrypt_t NUCC_Encrypt_original;
-
-std::uint64_t __fastcall NUCC_Encrypt(const char* a1) {
-    return NUCC_Encrypt_original(a1);
-}
-
-typedef float*(__fastcall* RGBA_Int_to_Float_t)(float*, unsigned int);
-RGBA_Int_to_Float_t RGBA_Int_to_Float_original;
-
-float* __fastcall RGBA_Int_to_Float(float* float_ref, unsigned int rgba) {
-    return RGBA_Int_to_Float_original(float_ref, rgba);
-}
-
-typedef float*(__fastcall* sub_47EB58_t)(std::uint64_t*, __uint128_t*, __uint128_t*);
-sub_47EB58_t sub_47EB58_original;
-
-float* __fastcall sub_47EB58(std::uint64_t* a1, __uint128_t* a2, __uint128_t* a3) {
-    return sub_47EB58_original(a1, a2, a3);
-}
-
-typedef std::uint64_t*(__fastcall* Parse_PlayerColorParam_t)(std::uint64_t*);
-Parse_PlayerColorParam_t Parse_PlayerColorParam_original;
-
-std::string to_hex_str(auto num) {
-    std::stringstream stream;
-    stream << std::hex << num;
-    return stream.str();
-}
-
-template<typename T>
-T get_offset_value(std::uint64_t* buffer, int size) {
-    return *reinterpret_cast<T*>(reinterpret_cast<std::uint8_t*>(buffer) + size);
-}
-template<typename T>
-T* get_offset_ptr(std::uint64_t* buffer, int size) {
-    return reinterpret_cast<T*>(reinterpret_cast<std::uint8_t*>(buffer) + size);
 }
 
 union U128 {
@@ -77,6 +27,18 @@ union U128 {
         
     };
 };
+
+template<typename T>
+T get_offset_value(std::uint64_t* buffer, int size) {
+    return *reinterpret_cast<T*>(reinterpret_cast<std::uint8_t*>(buffer) + size);
+}
+template<typename T>
+T* get_offset_ptr(std::uint64_t* buffer, int size) {
+    return reinterpret_cast<T*>(reinterpret_cast<std::uint8_t*>(buffer) + size);
+}
+
+typedef std::uint64_t*(__fastcall* Parse_PlayerColorParam_t)(std::uint64_t*);
+Parse_PlayerColorParam_t Parse_PlayerColorParam_original;
 
 std::uint64_t* __fastcall Parse_PlayerColorParam(std::uint64_t* a1) {
     std::uint64_t* buffer;
@@ -133,34 +95,6 @@ Hook Parse_PlayerColorParam_hook = {
     (void*)Parse_PlayerColorParam, // Address of our hook function
     (void**)&Parse_PlayerColorParam_original, // Address of the variable that will store the original function address
     "Parse_PlayerColorParam" // Name of the function we want to hook
-};
-
-Hook Load_nuccBinary_hook = {
-    (void*)0x671C30, // Address of the function we want to hook
-    (void*)Load_nuccBinary, // Address of our hook function
-    (void**)&Load_nuccBinary_original, // Address of the variable that will store the original function address
-    "Load_nuccBinary" // Name of the function we want to hook
-};
-
-Hook NUCC_Encrypt_hook = {
-    (void*)0x6C92A0, // Address of the function we want to hook
-    (void*)NUCC_Encrypt, // Address of our hook function
-    (void**)&NUCC_Encrypt_original, // Address of the variable that will store the original function address
-    "NUCC_Encrypt" // Name of the function we want to hook
-};
-
-Hook RGBA_Int_to_Float_hook = {
-    (void*)0x6DC840, // Address of the function we want to hook
-    (void*)RGBA_Int_to_Float, // Address of our hook function
-    (void**)&RGBA_Int_to_Float_original, // Address of the variable that will store the original function address
-    "RGBA_Int_to_Float" // Name of the function we want to hook
-};
-
-Hook sub_47EB58_hook = {
-    (void*)0x47EB58, // Address of the function we want to hook
-    (void*)sub_47EB58, // Address of our hook function
-    (void**)&sub_47EB58_original, // Address of the variable that will store the original function address
-    "sub_47EB58" // Name of the function we want to hook
 };
 
 // This function is called when the mod is loaded.
--- src/main.h
+++ src/main.h
@@ -3,6 +3,65 @@
 #define EXPORT extern "C" __declspec(dllexport)
 
 #include <JojoAPI.h>
+#include <cstdint>
 
 EXPORT ModMeta __stdcall GetModInfo();
 EXPORT void __stdcall ModInit();
+
+// Unmodified functions:
+
+typedef std::uint64_t*(__fastcall* Load_nuccBinary_t)(const char*, const char*);
+Load_nuccBinary_t Load_nuccBinary_original;
+
+std::uint64_t* __fastcall Load_nuccBinary(const char* a1, const char* a2) {
+    return Load_nuccBinary_original(a1, a2);
+}
+
+typedef std::uint64_t(__fastcall* NUCC_Encrypt_t)(const char*);
+NUCC_Encrypt_t NUCC_Encrypt_original;
+
+std::uint64_t __fastcall NUCC_Encrypt(const char* a1) {
+    return NUCC_Encrypt_original(a1);
+}
+
+typedef float*(__fastcall* RGBA_Int_to_Float_t)(float*, unsigned int);
+RGBA_Int_to_Float_t RGBA_Int_to_Float_original;
+
+float* __fastcall RGBA_Int_to_Float(float* float_ref, unsigned int rgba) {
+    return RGBA_Int_to_Float_original(float_ref, rgba);
+}
+
+typedef float*(__fastcall* sub_47EB58_t)(std::uint64_t*, __uint128_t*, __uint128_t*);
+sub_47EB58_t sub_47EB58_original;
+
+float* __fastcall sub_47EB58(std::uint64_t* a1, __uint128_t* a2, __uint128_t* a3) {
+    return sub_47EB58_original(a1, a2, a3);
+}
+
+Hook Load_nuccBinary_hook = {
+    (void*)0x671C30, // Address of the function we want to hook
+    (void*)Load_nuccBinary, // Address of our hook function
+    (void**)&Load_nuccBinary_original, // Address of the variable that will store the original function address
+    "Load_nuccBinary" // Name of the function we want to hook
+};
+
+Hook NUCC_Encrypt_hook = {
+    (void*)0x6C92A0, // Address of the function we want to hook
+    (void*)NUCC_Encrypt, // Address of our hook function
+    (void**)&NUCC_Encrypt_original, // Address of the variable that will store the original function address
+    "NUCC_Encrypt" // Name of the function we want to hook
+};
+
+Hook RGBA_Int_to_Float_hook = {
+    (void*)0x6DC840, // Address of the function we want to hook
+    (void*)RGBA_Int_to_Float, // Address of our hook function
+    (void**)&RGBA_Int_to_Float_original, // Address of the variable that will store the original function address
+    "RGBA_Int_to_Float" // Name of the function we want to hook
+};
+
+Hook sub_47EB58_hook = {
+    (void*)0x47EB58, // Address of the function we want to hook
+    (void*)sub_47EB58, // Address of our hook function
+    (void**)&sub_47EB58_original, // Address of the variable that will store the original function address
+    "sub_47EB58" // Name of the function we want to hook
+};
