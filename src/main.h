c87c0749cf9eab3258e7939485277fc039dd69b9
Kojo Bailey <kojomolojo@gmail.com>
2024-08-05 21:32:46 +0100

    Add back modified JAPI until official fix

--- src/main.h
+++ src/main.h
@@ -2,7 +2,6 @@
 
 #define EXPORT extern "C" __declspec(dllexport)
 
-#include "types.h"
 #include <JojoAPI.h>
 #include <nlohmann/json.hpp>
 
@@ -12,6 +11,17 @@
 
 namespace fs = std::filesystem;
 using JSON = nlohmann::ordered_json;
+
+using u128 = __uint128_t;
+using u64 = std::uint64_t;
+using u32 = std::uint32_t;
+using u16 = std::uint16_t;
+using u8 = std::uint8_t;
+using i128 = __int128_t;
+using i64 = std::int64_t;
+using i32 = std::int32_t;
+using i16 = std::int16_t;
+using i8 = std::int8_t;
 
 EXPORT ModMeta __stdcall GetModInfo();
 EXPORT void __stdcall ModInit();
